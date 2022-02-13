f9c9dc0081593b671ae4ab4f262c921050cff3df
niyobuhungiro ellen <89224993+niyoel@users.noreply.github.com>
2022-02-13 11:29:13 +0100

    Add files via upload

---
+++ juste_prix_v1.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+justPrix=randint(1,100)
+print(justPrix)
+print(" ----------------------------")
+print("| bienvenue sur notre plateu |")
+print(" ----------------------------")
+print("")
+while True:
+    prixDevine=int(input("devine la prix d'un objet: "))
+    if type(prixDevine)!=int:
+        print(" un nombre svp")
+    elif prixDevine < justPrix:
+        print("C’est moins")
+    elif prixDevine > justPrix:
+        print("c'est plus")
+    else: 
+        print(f"felicitation vous avez gagne le prix d'un obje est {justPrix}")
+        exit()
