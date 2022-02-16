3139eff98c1cdc89aab6582e5af8eb38d6c7bf9f
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-02-16 12:08:43 +0100

    modif

--- juste_prix_v1.py
+++ juste_prix_v1.py
@@ -20,19 +20,3 @@ while True:
 from random import randint
 
 
-justPrix=randint(1,100)
-print(justPrix)
-print(" ----------------------------")
-print("| bienvenue sur notre plateau |")
-print(" ----------------------------")
-print("")
-while True:
-    prixDevine=int(input("devine la prix d'un objet entre 1 et 100: "))
-    if prixDevine < justPrix:
-        print("C’est moins")
-    elif prixDevine > justPrix:
-        print("c'est plus")
-    else: 
-        print(f"felicitation vous avez gagne le prix d'un obje est {justPrix}")
->>>>>>> 514e0f2bb21d74526a4885894d0759c336d45136
-        exit()
