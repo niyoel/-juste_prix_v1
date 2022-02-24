56923f3b7dff06947525bc629a9cd5a0725c5af1
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-02-24 16:57:35 +0100

    MODIF

--- juste_prix_v2.py
+++ juste_prix_v2.py
@@ -1,57 +1,27 @@
-<<<<<<< HEAD
+
 from random import randint
-from turtle import clear
 juste_prix=randint(1,100)
-print(juste_prix)
+# print(juste_prix)
 print(" ----------------------------")
 print("| Bienvenue sur notre plateau |")
 print(" ----------------------------")
 print("")
-i=1
-j=9
-while i<=10:
-        prix_devine=int(input("Devinez le prix d'un objet entre 1 et 100. Attention! Vous avez 10 essais seulement "))
+loop_count=1
+essais_count=9
+while loop_count<=10:
+        prix_devine=int(input(f"Devinez le prix d'un objet entre 1 et 100. Attention! Vous avez {essais_count+1} seulement: "))
         if prix_devine < juste_prix:
-            print(f"C'est plus, vous avez encore {j} essais" )
-            i=i+1
-            j=j-1
+            print("C'est plus")
+            loop_count=loop_count+1
+            essais_count=essais_count-1
         elif prix_devine > juste_prix:
-            print(f"C'est moins, vous avez encore {j} essais")
-            i=i+1
-            j=j-1
+            print("C'est moins")
+            loop_count=loop_count+1
+            essais_count=essais_count-1
         else:
-            print(f"Félicitations vous avez gagne  à la {i}éme essais  le juste prix est: {juste_prix}")
+            print(f"Félicitations vous avez gagne  à la {loop_count}éme essais  le juste prix est: {juste_prix}")
             exit()
     
 print(f"La partie est terminée. Vous avez echoué! Le prix est:{juste_prix} ")
             
  
-=======
-from random import randint
-from turtle import clear
-juste_prix=randint(1,100)
-print(juste_prix)
-print(" ----------------------------")
-print("| Bienvenue sur notre plateau |")
-print(" ----------------------------")
-print("")
-i=1
-j=9
-while i<=10:
-        prix_devine=int(input("Devinez le prix d'un objet entre 1 et 100. Attention! Vous avez 10 essais seulement "))
-        if prix_devine < juste_prix:
-            print(f"C'est plus, vous avez encore {j} essais" )
-            i=i+1
-            j=j-1
-        elif prix_devine > juste_prix:
-            print(f"C'est moins, vous avez encore {j} essais")
-            i=i+1
-            j=j-1
-        else:
-            print(f"Félicitations vous avez gagne  à la {i}éme essais  le juste prix est: {juste_prix}")
-            exit()
-    
-print(f"La partie est terminée. Vous avez echoué! Le prix est:{juste_prix} ")
-            
- 
->>>>>>> 8987bb1d99dcea7ba70f1c8a41cb31e63a85378e
