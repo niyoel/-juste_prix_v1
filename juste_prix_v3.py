9b23b4a67539f1814fd43a2b536827ba3a684156
niyobuhungiro ellen <ellenniyobuhungiro01@gmail.com>
2022-02-25 08:30:08 +0100

    modif

--- juste_prix_v3.py
+++ juste_prix_v3.py
@@ -40,7 +40,7 @@ if mode=="P":
    print ("Entrez mx pour choisir le prix maximale que tu peux devine","nbr pour le nombre d'essais que tu veux", sep="\n")
    choix=str(input("-"))
    if choix=="mx":
-        max= int(input("Choisissez la limite de prix: "))
+        max= int(input("Choisissez le prix maximal d'un objet: "))
         juste_prix=randint(1,max)
         while True:
             prix_devine=int(input(f"Devinez le prix d'un objet entre 1 et {max}: "))
@@ -52,7 +52,7 @@ if mode=="P":
                 print(f"Félicitation vous avez gagne le prix d'un objet  {juste_prix}")
                 exit()
    if choix=="nbr":
-        nombre=int(input("Choisissez le nombre d'essais: "))
+        nombre=int(input("Choisissez le nombre d'essais que vous voulais: "))
         if nombre==0:
             juste_prix=randint(1,100)
             while True:
@@ -66,21 +66,19 @@ if mode=="P":
                     exit()
         else:
             juste_prix=randint(1,100)
-            loop_count=nombre
+            loop_count=1
             essais_count=nombre-1
-            while loop_count<=nombre:
-                prix_devine=int(input(f"Devinez le prix d'un objet entre 1 et 100. Attention! Vous avez {essais_count+1} seulement: "))
-                if prix_devine < juste_prix:
-                    print("C'est plus")
-                    loop_count=loop_count+1
-                    essais_count=essais_count-1
-                elif prix_devine > juste_prix:
-                    print("C'est moins")
-                    loop_count=loop_count+1
-                    essais_count=essais_count-1
-                else:
-                    print(f"Félicitations vous avez gagne  à la {loop_count}éme essais  le juste prix est: {juste_prix}")
-                    exit()
+            while loop_count<= nombre:
+                    prix_devine=int(input(f"Devinez le prix d'un objet entre 1 et 100. Attention! Vous avez {essais_count+1} seulement: "))
+                    if prix_devine < juste_prix:
+                        print("C'est plus")
+                        loop_count=loop_count+1
+                        essais_count=essais_count-1
+                    elif prix_devine > juste_prix:
+                        print("C'est moins")
+                        loop_count=loop_count+1
+                        essais_count=essais_count-1
+                    else:
+                        print(f"Félicitations vous avez gagne  à la {loop_count}éme essais  le juste prix est: {juste_prix}")
+                        exit()
             print(f"La partie est terminée. Vous avez echoué! Le prix est:{juste_prix} ")
-
-            
